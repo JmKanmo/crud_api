17e7fa9d5c3b83cbaee0ec2a2145703609c322c5
JunmoKang <nebi25@ex-em.com>
2020-08-06 13:01:22 +0900

    put method logic code 작성

--- src/main/java/model/sql/sqlCommand.java
+++ src/main/java/model/sql/sqlCommand.java
@@ -5,4 +5,5 @@ public class DaoSqls {
     public static final String SELECT_ALL_USER = "SELECT* FROM userinfo;";
     public static final String SELECT_USER = "SELECT*  FROM userinfo WHERE id = ?";
     public static final String DELETE_USER = "DELETE FROM userinfo where id = ?";
+    public static final String UPDATE_USER =  "UPDATE userinfo SET name ";
 }
